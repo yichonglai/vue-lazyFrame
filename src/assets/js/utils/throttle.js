7b6a8c267cd22d894c306c43a499b711cf583b70
nklstYI <1452758010@qq.com>
2019-07-26 16:53:14 +0800

    必报添加一个节流函数

---
+++ src/assets/js/utils/throttle.js
@@ -0,0 +1,18 @@
+function throttle (fn, delay) {
+  // 利用闭包保存时间
+  let prev = Date.now()
+  return function () {
+    let context = this
+    let arg = arguments
+    let now = Date.now()
+    if (now - prev >= delay) {
+      fn.apply(context, arg)
+      prev = Date.now()
+    }
+  }
+}
+
+function fn () {
+  console.log('节流')
+}
+addEventListener('scroll', throttle(fn, 1000))
